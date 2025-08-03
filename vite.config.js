0bd366f68cad7b5ec3044ba269502fa6d2ee89f0
Prajwal-u2 <prajwal.u.34@gmail.com>
2025-08-03 05:01:56 +0000

    fix deploy

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/personal-portfolio/',
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 }) 
